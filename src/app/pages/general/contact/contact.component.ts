083a2626a5c780bfe27049aad8b24cd63490f06c
Administrator <balanatesh3@gmail.com>
2024-09-19 09:26:28 +0530

    contact updated

--- src/app/pages/general/contact/contact.component.ts
+++ src/app/pages/general/contact/contact.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
